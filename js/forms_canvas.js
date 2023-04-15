112d6743c2e1bcff64642335a2a421ec05721943
juniorkiobr <juniorkiolouco66@gmail.com>
2023-04-14 21:09:12 -0300

    Separando a lógica do jogo da de desenho em cavas.

--- js/forms_canvas.js
+++ js/forms_canvas.js
@@ -33,157 +33,19 @@ var colors = {
     gold: "gold"
 }
 
-var jogo = {
-    _canvas: null,
+var formas = {
     _pincel: null,
-    _erros: 0,
-    _palavra: "",
-    _tentadas: [],
-    _exibidas: [],
-    _acertos: [],
-    _gameOver: false,
-    _dicionario: null,
+    _canvas: null,
+    width: 0,
+    height: 0,
     set_canvas: function (canvas, width, height) {
         this._canvas = canvas;
+        this.width = width;
+        this.height = height;
         this._canvas.width = width;
         this._canvas.height = height;
         this._pincel = this._canvas.getContext("2d");
-
-    },
-    tentaPalavra: function (letra) {
-
-        if (this._palavra.indexOf(letra) != -1 && this._acertos.indexOf(letra) == -1) {
-            this._acertos.push(letra);
-            letra = '<verde>' + letra + "</verde>";
-        } else {
-            this._erros++;
-            letra = '<vermelho>' + letra + "</vermelho>";
-        }
-        this._tentadas.push(letra);
-
-        // this.update();
-    },
-    desenhaForca: function (x, y, cor) {
-        jogo.desenhaRetangulo(x, y, 4, 100, cor);
-        jogo.desenhaRetangulo(x, y, 55, 4, cor);
-        jogo.desenhaRetangulo(70, y, 4, 20, cor);
-    },
-    definirPalavra: async function () {
-        if ((this._palavra == null || this._palavra == "") && !this._gameOver && !this._carregando) {
-            this._carregando = true;
-
-            let palavra = await fetch("https://api.dicionario-aberto.net/random");
-            let json = await palavra.json();
-            console.log(json);
-            console.log(json.word);
-            this._dicionario = await getSignificado(json.word);
-
-            if (json.word != null && json.word != "" && json.word.length <= 12 && (jogo._palavra == "" || jogo._palavra == null)) {
-                let tmp = json.word.toLowerCase();
-
-                // Linha para remover acentos
-                tmp = tmp.normalize("NFD").replace(/([\u0300-\u036f])+/g, "");
-                console.log(tmp);
-
-                jogo._palavra = tmp;
-            }
-
-            this._carregando = false;
-            // palavra.then(function (response) {
-            //     // console.log(response);
-            //     response.json().then(function (json) {
-
-            //     });
-            // });
-        }
-    },
-    desenhaPalavra: function () {
-        let inicioX = 95;
-        let inicioY = 180;
-        let tamanho = 30;
-        let espaco = 10;
-        for (let i = 0; i < this._palavra.length; i++) {
-            this._exibidas[i] = "_";
-            // const element = palavra[i];
-            jogo.desenhaRetangulo(inicioX, inicioY, tamanho, 3, colors.black);
-            if (this._acertos.indexOf(this._palavra[i]) != -1) {
-                this.desenhaTexto(this._palavra[i], inicioX + tamanho / 2, inicioY - 10, colors.black, "bold 20px Arial", "center");
-                this._exibidas[i] = this._palavra[i];
-            }
-            inicioX += tamanho + espaco;
-
-        }
-    },
-    desenhaErros: function () {
-        let valores = [
-            [71, 118, 10, colors.red, 0],
-            [70, 127, 4, 25, colors.red, 0],
-            [70, 135, 20, 4, colors.red, 40],
-            [73, 135, -20, 4, colors.red, -40],
-            [70, 155, 20, 4, colors.red, 40],
-            [73, 155, -20, 4, colors.red, -40]
-        ];
-        if (this._erros > valores.length) {
-            this._erros = valores.length;
-        }
-
-        for (let i = 0; i < this._erros; i++) {
-            let valor = valores[i];
-            if (i == 0) {
-                this.desenhaCirculo(valor[0], valor[1], valor[2], valor[3]);
-            } else {
-                this.desenhaRetangulo(valor[0], valor[1], valor[2], valor[3], valor[4], valor[5]);
-            }
-        }
-    },
-    update: async function () {
-        await this.definirPalavra();
-
-        if (!this._gameOver && this._palavra != null && this._palavra != "") {
-            let palavraTentada = document.getElementById("palavrasTentadas");
-
-            this.desenhaRetangulo(0, 0, this._canvas.width, this._canvas.height, colors.white);
-            this.desenhaForca(15, 100, colors.black);
-            this.desenhaErros();
-            this.desenhaPalavra(this._palavra);
-            palavraTentada.innerHTML = "Palavras tentadas:. " + this._tentadas.join(", ");
-            if (this._exibidas.join("") == this._palavra) {
-                this._gameOver = true;
-                this.desenhaRetangulo(0, 0, this._canvas.width, this._canvas.height, colors.black);
-                this.desenhaTexto("Você ganhou!", this._canvas.width / 2 - 50, this._canvas.height / 2, colors.white, "bold 20px Arial", "center");
-                this.desenhaTexto("A palavra era: " + this._palavra, this._canvas.width / 2 - 50, this._canvas.height / 2 + 25, colors.white, "bold 20px Arial", "center");
-                this.desenhaTexto("Aperte 'Enter' jogar novamente", this._canvas.width / 2 - 50, this._canvas.height / 2 + 50, colors.white, "bold 20px Arial", "center");
-                fillSignificado(this._dicionario);
-
-
-            } else if (this._erros >= 6) {
-                this._gameOver = true;
-                this.desenhaRetangulo(0, 0, this._canvas.width, this._canvas.height, colors.black);
-                this.desenhaTexto("Você perdeu!", this._canvas.width / 2 - 50, this._canvas.height / 2, colors.white, "bold 20px Arial", "center");
-                this.desenhaTexto("A palavra era: " + this._palavra, this._canvas.width / 2 - 50, this._canvas.height / 2 + 25, colors.white, "bold 20px Arial", "center");
-                this.desenhaTexto("Aperte 'Enter' jogar novamente", this._canvas.width / 2 - 50, this._canvas.height / 2 + 50, colors.white, "bold 20px Arial", "center");
-                fillSignificado(this._dicionario);
-
-
-            }
-        }
-
-    },
-    resetGame: function () {
-        this._gameOver = false;
-        this._erros = 0;
-        this._acertos = [];
-        this._exibidas = [];
-        this._palavra = "";
-        this._dicionario = "";
-        let significado = document.getElementById("significado");
-        significado.innerHTML = "";
-        let palavra = document.getElementById("palavra");
-        palavra.innerHTML = "";
-        let etimologia = document.getElementById("etimologia");
-        etimologia.innerHTML = "";
-        this._tentadas = [];
-        let palavrasTentadas = document.getElementById("palavrasTentadas");
+        return this;
 
     },
     desenhaRetangulo: function (x, y, largura, altura, cor, angulo = 0) {
---
+++ js/jogo.js
@@ -0,0 +1,152 @@
+var jogo = {
+    _canvas: null,
+    _erros: 0,
+    _palavra: "",
+    _tentadas: [],
+    _exibidas: [],
+    _acertos: [],
+    _gameOver: false,
+    _dicionario: null,
+    set_canvas: function (canvas, width, height) {
+
+        this._canvas = formas.set_canvas(canvas, width, height);
+
+    },
+    tentaPalavra: function (letra) {
+
+        if (this._palavra.indexOf(letra) != -1 && this._acertos.indexOf(letra) == -1) {
+            this._acertos.push(letra);
+            letra = '<verde>' + letra + "</verde>";
+        } else {
+            this._erros++;
+            letra = '<vermelho>' + letra + "</vermelho>";
+        }
+        this._tentadas.push(letra);
+
+        // this.update();
+    },
+    desenhaForca: function (x, y, cor) {
+        jogo._canvas.desenhaRetangulo(x, y, 4, 100, cor);
+        jogo._canvas.desenhaRetangulo(x, y, 55, 4, cor);
+        jogo._canvas.desenhaRetangulo(70, y, 4, 20, cor);
+    },
+    definirPalavra: async function () {
+        if ((this._palavra == null || this._palavra == "") && !this._gameOver && !this._carregando) {
+            this._carregando = true;
+
+            let palavra = await fetch("https://api.dicionario-aberto.net/random");
+            let json = await palavra.json();
+            console.log(json);
+            console.log(json.word);
+            this._dicionario = await getSignificado(json.word);
+
+            if (json.word != null && json.word != "" && json.word.length <= 12 && (jogo._palavra == "" || jogo._palavra == null)) {
+                let tmp = json.word.toLowerCase();
+
+                // Linha para remover acentos
+                tmp = tmp.normalize("NFD").replace(/([\u0300-\u036f])+/g, "");
+                console.log(tmp);
+
+                jogo._palavra = tmp;
+            }
+
+            this._carregando = false;
+            // palavra.then(function (response) {
+            //     // console.log(response);
+            //     response.json().then(function (json) {
+
+            //     });
+            // });
+        }
+    },
+    desenhaPalavra: function () {
+        let inicioX = 95;
+        let inicioY = 180;
+        let tamanho = 30;
+        let espaco = 10;
+        for (let i = 0; i < this._palavra.length; i++) {
+            this._exibidas[i] = "_";
+            // const element = palavra[i];
+            jogo._canvas.desenhaRetangulo(inicioX, inicioY, tamanho, 3, colors.black);
+            if (this._acertos.indexOf(this._palavra[i]) != -1) {
+                this._canvas.desenhaTexto(this._palavra[i], inicioX + tamanho / 2, inicioY - 10, colors.black, "bold 20px Arial", "center");
+                this._exibidas[i] = this._palavra[i];
+            }
+            inicioX += tamanho + espaco;
+
+        }
+    },
+    desenhaErros: function () {
+        let valores = [
+            [71, 118, 10, colors.red, 0],
+            [70, 127, 4, 25, colors.red, 0],
+            [70, 135, 20, 4, colors.red, 40],
+            [73, 135, -20, 4, colors.red, -40],
+            [70, 155, 20, 4, colors.red, 40],
+            [73, 155, -20, 4, colors.red, -40]
+        ];
+        if (this._erros > valores.length) {
+            this._erros = valores.length;
+        }
+
+        for (let i = 0; i < this._erros; i++) {
+            let valor = valores[i];
+            if (i == 0) {
+                this._canvas.desenhaCirculo(valor[0], valor[1], valor[2], valor[3]);
+            } else {
+                this._canvas.desenhaRetangulo(valor[0], valor[1], valor[2], valor[3], valor[4], valor[5]);
+            }
+        }
+    },
+    update: async function () {
+        await this.definirPalavra();
+
+        if (!this._gameOver && this._palavra != null && this._palavra != "") {
+            let palavraTentada = document.getElementById("palavrasTentadas");
+
+            this._canvas.desenhaRetangulo(0, 0, this._canvas.width, this._canvas.height, colors.white);
+            this.desenhaForca(15, 100, colors.black);
+            this.desenhaErros();
+            this.desenhaPalavra(this._palavra);
+            palavraTentada.innerHTML = "Palavras tentadas:. " + this._tentadas.join(", ");
+            if (this._exibidas.join("") == this._palavra) {
+                this._gameOver = true;
+                this._canvas.desenhaRetangulo(0, 0, this._canvas.width, this._canvas.height, colors.black);
+                this._canvas.desenhaTexto("Você ganhou!", this._canvas.width / 2 - 50, this._canvas.height / 2, colors.white, "bold 20px Arial", "center");
+                this._canvas.desenhaTexto("A palavra era: " + this._palavra, this._canvas.width / 2 - 50, this._canvas.height / 2 + 25, colors.white, "bold 20px Arial", "center");
+                this._canvas.desenhaTexto("Aperte 'Enter' jogar novamente", this._canvas.width / 2 - 50, this._canvas.height / 2 + 50, colors.white, "bold 20px Arial", "center");
+                fillSignificado(this._dicionario);
+
+
+            } else if (this._erros >= 6) {
+                this._gameOver = true;
+                this._canvas.desenhaRetangulo(0, 0, this._canvas.width, this._canvas.height, colors.black);
+                this._canvas.desenhaTexto("Você perdeu!", this._canvas.width / 2 - 50, this._canvas.height / 2, colors.white, "bold 20px Arial", "center");
+                this._canvas.desenhaTexto("A palavra era: " + this._palavra, this._canvas.width / 2 - 50, this._canvas.height / 2 + 25, colors.white, "bold 20px Arial", "center");
+                this._canvas.desenhaTexto("Aperte 'Enter' jogar novamente", this._canvas.width / 2 - 50, this._canvas.height / 2 + 50, colors.white, "bold 20px Arial", "center");
+                fillSignificado(this._dicionario);
+
+
+            }
+        }
+
+    },
+    resetGame: function () {
+        this._gameOver = false;
+        this._erros = 0;
+        this._acertos = [];
+        this._exibidas = [];
+        this._palavra = "";
+        this._dicionario = "";
+        let significado = document.getElementById("significado");
+        significado.innerHTML = "";
+        let palavra = document.getElementById("palavra");
+        palavra.innerHTML = "";
+        let etimologia = document.getElementById("etimologia");
+        etimologia.innerHTML = "";
+        this._tentadas = [];
+        let palavrasTentadas = document.getElementById("palavrasTentadas");
+
+    },
+
+};	
